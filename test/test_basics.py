bca3d67e669edc7457ed0a0ac488f399ea72d8ca
David Branner <brannerchinese@gmail.com>
2014-09-25 11:33:49 -0400

    update test_basics.py

---
+++ test/test_basics.py
@@ -0,0 +1,99 @@
+# test_basics.py
+# David Prager Branner
+
+
+
+import bidict as B
+import pytest
+import random
+import string
+
+def random_ints(cardinality=10, hi=10, lo=-10):
+    """return list of `cardinality` ints, each in range (lo, hi)."""
+    if cardinality > hi - lo:
+        raise Exception(
+                'cardinality ({}) > hi - lo ({}); impossible condition'.
+                format(cardinality, hi-lo))
+    results = set()
+    while len(results) < cardinality:
+        results.add( random.randint(lo, hi))
+    return results
+
+def random_strings(length=10, cardinality=10):
+    """return list of `cardinality` strings, each length `length`."""
+    results = set()
+    while len(results) < cardinality:
+        results.add(''.join(
+                random.choice(string.ascii_letters + string.digits) 
+                for i in range(length)))
+    return results
+
+def test_basic_getitem():
+    element_by_symbol = B.bidict(H='hydrogen')
+    element_by_symbol['H'] = 'hydrogen'
+    assert element_by_symbol['H':] == 'hydrogen'
+    assert element_by_symbol[:'hydrogen'] == 'H'
+
+def test_delete():
+    element_by_symbol = B.bidict(H='hydrogen')
+    element_by_symbol['He':] = 'helium'
+    element_by_symbol[:'lithium'] = 'Li'
+    del element_by_symbol['H':]
+    del element_by_symbol[:'lithium']
+    assert element_by_symbol == B.bidict({'He': 'helium'})
+
+def test_in_01():
+    element_by_symbol = B.bidict(He='helium')
+    assert bool('C' in element_by_symbol) == False
+
+def test_in_02():
+    element_by_symbol = B.bidict(Hg='mercury')
+    assert bool('mercury' in ~element_by_symbol) == True
+
+def test_get():
+    element_by_symbol = B.bidict(He='helium')
+    assert element_by_symbol.get('C', 'carbon') == 'carbon'
+
+def test_pop_01():
+    element_by_symbol = B.bidict(He='helium')
+    assert element_by_symbol.pop('He') == 'helium'
+
+def test_pop_02():
+    element_by_symbol = B.bidict(He='helium')
+    element_by_symbol.pop('He')
+    assert element_by_symbol == B.bidict({})
+
+def test_pop_03():
+    element_by_symbol = B.bidict(Hg='mercury')
+    assert (~element_by_symbol).pop('mercury') == 'Hg'
+
+def test_update():
+    element_by_symbol = B.bidict()
+    element_by_symbol.update(Hg='mercury')
+    assert element_by_symbol == B.bidict({'Hg': 'mercury'})
+
+def test_unary_inverse():
+    element_by_symbol = B.bidict(Hg='mercury')
+    assert ~element_by_symbol == B.bidict({'mercury': 'Hg'})
+
+def test_unhashable_list():
+    with pytest.raises(TypeError):
+        anagrams_by_alphagram = B.bidict(opt=['opt', 'pot', 'top'])
+
+def test_unhashable_sublist():
+    sublist = (1, 2, '3', ('r', [4]), 5)
+    with pytest.raises(TypeError):
+        anagrams_by_alphagram = B.bidict(sublist)
+
+def test_hashable_tuple():
+    anagrams_by_alphagram = B.bidict(opt=('opt', 'pot', 'top'))
+    assert len(anagrams_by_alphagram) == 1
+
+def test_like_value_differing_keys_01():
+    nils = B.bidict(zero=0, zilch=0, zip=0)
+    assert len(nils) == 1
+
+def test_like_value_differing_keys_02():
+    b = B.bidict({1: 'one', 2: 'two'})
+    b[:'two'] = 1
+    assert b == B.bidict({1: 'two'})
