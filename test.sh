c4ae97dd8b5b59f49dad4b9572f0b3167005148c
jab <jab@users.noreply.github.com>
2016-01-13 16:54:29 +0000

    several improvements to test setup
    - use latest patch releases of all python versions via pyenv
    - use caching to speed up travis builds
    - peg to latest versions of test deps (including hypothesis 2.0)
    - set hypothesis to strict
    - explicitly set average_size on strategies that accept it
    - drop pypy3, not worth supporting till it catches up to cpython
    - only pass --cov for python27 and 35; with other versions, it slows down
      hypothesis data generation so much, the health checks fail
    - refactor tox and travis setup to use new test.sh helper script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# this is what .travis.yml and tox.ini use to run tests
+
+set -ev
+
+COV="--cov=bidict"
+# With hypothesis>=1.19.0,
+# data generation is so slow when using --cov
+# that it can cause health checks to fail
+# in slow environments such as Travis-CI
+# with certain Python versions such as pypy.
+# Don't pass --cov in these cases:
+[[ $TRAVIS_PYTHON_VERSION =~ ^(3\.3|3\.4|pypy)$ ]] && COV=""
+py.test $COV || FAILED=1
+pep257 bidict || FAILED=1
+exit $FAILED
