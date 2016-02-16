3047e60291f6cb304c53094a97f67efd09501b1f
jab <jab@users.noreply.github.com>
2016-02-16 22:00:27 +0000

    tweak docstrings
    [ci skip]

--- bidict/_bidict.py
+++ bidict/_bidict.py
@@ -26,20 +26,18 @@ class bidict(BidirectionalMapping, MutableMapping):
         If *key* is already associated with a different value,
         the old value will be replaced with *val*,
         as with :py:meth:`dict.__setitem__`.
-        In other words, value-overwriting modifications are allowed.
 
         If *val* is already associated with a different key,
         an exception is raised
-        to protect against accidentally replacing the existing mapping.
-        In other words, key-overwriting modifications are not allowed.
+        to protect against accidental removal of the key
+        that's currently associated with *val*.
 
-        You can use :attr:`put` instead
-        if you want to specify custom key- or value-overwriting behavior,
-        or use :attr:`forceput` to unconditionally associate *key* with *val*,
-        replacing any existing mappings as necessary to preserve uniqueness.
+        Use :attr:`put` instead if you want to specify different behavior in
+        the case that the provided key or value collides with an existing one.
+        Or use :attr:`forceput` to unconditionally associate *key* with *val*,
+        replacing any existing key or value as necessary to preserve uniqueness.
 
-        :raises bidict.ValueExistsException: if attempting to set a mapping
-            with a non-unique value.
+        :raises bidict.ValueExistsException: if *val* is not unique.
         """
         self._put(key, val, OVERWRITE, RAISE)
 
@@ -56,12 +54,12 @@ class bidict(BidirectionalMapping, MutableMapping):
 
         If *key* is already associated with *val*, this is a no-op.
 
-        :raises bidict.KeyExistsException: if attempting to insert a mapping
-            with the same key as an existing mapping, and *key_clbhv* is
+        :raises bidict.KeyExistsException: if attempting to insert an item
+            whose key collides with an existing item's, and *key_clbhv* is
             :attr:`CollisionBehavior.RAISE <bidict.CollisionBehavior.RAISE>`.
 
-        :raises bidict.ValueExistsException: if attempting to insert a mapping
-            with the same value as an existing mapping, and *val_clbhv* is
+        :raises bidict.ValueExistsException: if attempting to insert an item
+            whose value collides with an existing item's, and *val_clbhv* is
             :attr:`CollisionBehavior.RAISE <bidict.CollisionBehavior.RAISE>`.
         """
         self._put(key, val, key_clbhv, val_clbhv)
@@ -107,29 +105,20 @@ class bidict(BidirectionalMapping, MutableMapping):
         return self[key]
 
     def update(self, *args, **kw):
-        """
-        Like an atomic bulk :attr:`__setitem__`.
-
-        If adding any of the items given in *args* or *kw* would cause a
-        :class:`ValueExistsException <bidict.ValueExistsException>`,
-        none of them will be added.
-
-        :raises bidict.ValueExistsException: if attempting to insert a mapping
-            with a non-unique value.
-        """
+        """Like a bulk :attr:`__setitem__`."""
         self._update(OVERWRITE, RAISE, *args, **kw)
 
     def forceupdate(self, *args, **kw):
-        """Like :attr:`bidict.update <bidict.bidict.update>`, with OVERWRITE collision behavior."""
+        """Like a bulk :attr:`forceput`."""
         self._update(OVERWRITE, OVERWRITE, *args, **kw)
 
     def putall(self, key_clbhv, val_clbhv, *args, **kw):
         """
-        Like an atomic bulk :attr:`put` with the specified collision behaviors.
+        Like a bulk :attr:`put`.
 
-        If adding any of the items given in *args* or *kw* would cause a
-        :class:`KeyExistsException <bidict.ValueExistsException>` or
-        :class:`ValueExistsException <bidict.ValueExistsException>`,
+        If adding any of the items in *args* or *kw*
+        using the specified collision behaviors
+        would cause an exception,
         none of them will be added.
         """
         self._update(key_clbhv, val_clbhv, *args, **kw)
