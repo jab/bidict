1f723b63f30ce062c8c20ca41159d030cf49ecf0
jab <jab@users.noreply.github.com>
2016-06-15 16:13:14 +0000

    run sphinx via sphinx-build not make

--- test.sh
+++ test.sh
@@ -5,9 +5,7 @@ set -ev
 
 flake8 bidict tests/*.py || { EXIT=1 && echo flake8 failed; }
 pydocstyle bidict || { EXIT=1 && echo pydocstyle failed; }
-cd docs
-make html linkcheck || { EXIT=1 && echo docsbuild failed; }
-cd -
+sphinx-build -n -b html -b linkcheck -d docs/_build/doctrees docs docs/_build/html || { EXIT=1 && echo docsbuild failed; }
 
 test -z "$BIDICT_COVERAGE_DISABLE" && COV="--cov=bidict"
 test -n "$BENCHMARK_DIR" && BENCHMARK_STORAGE="--benchmark-storage=$BENCHMARK_DIR"
