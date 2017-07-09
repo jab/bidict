72b0928952eddbe6741671f96cb0992b4f75080c
jab <jab@users.noreply.github.com>
2017-07-09 03:05:35 +0000

    update to latest hypothesis (3.12.0)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = from_file('README.rst', 'See https://bidict.readthedocs.org')
 tests_require = [
     'coverage==4.4.1',
     'flake8==3.2.1',
-    'hypothesis==3.11.6',
+    'hypothesis==3.12.0',
     'hypothesis-pytest==0.19.0',
     'py==1.4.34',
     'pydocstyle==2.0.0',
