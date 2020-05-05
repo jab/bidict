2d571c486b77023139a0b99d96e223c9241accf4
timpaquatte <timothee.paquatte@polytechnique.edu>
2020-05-05 10:32:28 +0200

    Add some properties to spot the mutants

--- tests/properties/test_properties.py
+++ tests/properties/test_properties.py
@@ -402,3 +402,47 @@ def test_inverted_orderedbidict(ob_and_od):
     od_inv = OrderedDict((v, k) for (k, v) in od.items())
     assert all(i == j for (i, j) in zip(inverted(ob), od_inv.items()))
     assert all(i == j for (i, j) in zip(inverted(inverted(ob)), od.items()))
+
+from hypothesis import assume
+from bidict._dup import ON_DUP_DROP_OLD
+
+def test_isinv_correct():
+    ElementMap = namedbidict("ElementMap", "symbol", "name")
+    noble_gases = ElementMap()
+    noble_gases["He"] = "helium"
+
+    assert noble_gases.name_for['He'] == "helium"
+    assert noble_gases.symbol_for['helium'] == "He"
+
+@given(st.MUTABLE_BIDICTS, st.PAIRS)
+def test_put_correct(mb, pair):
+    key, val = pair
+    copy = mb.copy()
+    mb.put(key, val, ON_DUP_DROP_OLD)
+    if (key, val) in copy.items():
+        assert copy == mb
+    else:
+        assert copy != mb
+
+@given(st.MUTABLE_BIDICTS)
+def test_clear_correct(mb):
+    mb.clear()
+    assert len(mb.items()) == 0
+
+@given(st.MUTABLE_BIDICTS, st.PAIRS)
+def test_update_correct(mb, pair):
+    key, value = pair
+    assume(key not in mb.keys())
+    assume(value not in mb.values())
+
+    mb.update([pair])
+    assert mb[key] == value
+
+@given(st.MUTABLE_BIDICTS)
+def test_del_correct(mb):
+    assume(len(mb.items()) > 0)
+    from random import choice
+    rand_key = choice(list(mb.keys()))
+
+    del mb[rand_key]
+    assert not rand_key in mb
