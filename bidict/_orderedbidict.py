9e578eee0a907817173f1277c1c7d4a8cdf6f0fa
Joshua Bronson <jabronson@gmail.com>
2021-02-04 00:23:03 +0000

    Update pre-commit hooks
    Also remove no-longer-needed `type: ignore` comment as of latest mypy.

--- bidict/_orderedbidict.py
+++ bidict/_orderedbidict.py
@@ -60,7 +60,7 @@ class OrderedBidict(OrderedBidictBase[KT, VT], MutableBidict[KT, VT]):
         """
         if not self:
             raise KeyError('mapping is empty')
-        key = next((reversed if last else iter)(self))  # type: ignore
+        key = next((reversed if last else iter)(self))
         val = self._pop(key)
         return key, val
 
