89a654a0fbbf129ffdc42b23064cec6abddef3e8
Joshua Bronson <jabronson@gmail.com>
2021-11-04 20:07:14 +0000

    Bump version post-release

--- bidict/metadata.py
+++ bidict/metadata.py
@@ -8,7 +8,7 @@
 """Define bidict package metadata."""
 
 
-__version__ = '0.21.4'
+__version__ = '0.21.5.dev0'
 __author__ = 'Joshua Bronson'
 __maintainer__ = 'Joshua Bronson'
 __copyright__ = 'Copyright 2009-2021 Joshua Bronson'
