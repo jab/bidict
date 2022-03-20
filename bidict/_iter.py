5956a19ccda75168269768d2fd6eccb9257bfb48
Joshua Bronson <jabronson@gmail.com>
2022-03-20 14:06:08 +0000

    Upgrade to latest Pyright

--- bidict/_iter.py
+++ bidict/_iter.py
@@ -18,7 +18,7 @@ def iteritems_mapping_or_iterable(arg: MapOrIterItems[KT, VT]) -> IterItems[KT, 
     yield from arg.items() if isinstance(arg, t.Mapping) else arg  # pyright: ignore
 
 
-def iteritems(__arg: MapOrIterItems[KT, VT], **kw: VT) -> IterItems[KT, VT]:  # pyright: ignore [reportMissingParameterType]
+def iteritems(__arg: MapOrIterItems[KT, VT], **kw: VT) -> IterItems[KT, VT]:
     """Yield the items from *arg* and then any from *kw* in the order given."""
     yield from iteritems_mapping_or_iterable(__arg)
     yield from kw.items()  # type: ignore [misc]
