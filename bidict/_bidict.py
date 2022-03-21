6cb30f789d6b8d3f1615dcb11b8b9d6589d0ee6e
Joshua Bronson <jabronson@gmail.com>
2022-03-21 11:48:22 +0000

    Better workaround for python/mypy#12390

--- bidict/_bidict.py
+++ bidict/_bidict.py
@@ -105,10 +105,10 @@ class MutableBidict(BidictBase[KT, VT], MutableBidirectionalMapping[KT, VT]):
         self._fwdm.clear()
         self._invm.clear()
 
-    @t.overload  # type: ignore [override]  # https://github.com/python/mypy/issues/12390
+    @t.overload
     def pop(self, __key: KT) -> VT: ...
     @t.overload
-    def pop(self, __key: KT, __default: DT) -> t.Union[VT, DT]: ...
+    def pop(self, __key: KT, __default: DT = ...) -> t.Union[VT, DT]: ...
 
     def pop(self, key: KT, default: ODT[DT] = MISSING) -> t.Union[VT, DT]:
         """*x.pop(k[, d]) → v*
