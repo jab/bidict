b0d602ea06e55ecb9bbc6de9c9b85cf0558e39d5
Joshua Bronson <jabronson@gmail.com>
2022-12-31 20:58:02 -0500

    Happy 2023 + bump version post-release

--- bidict/metadata.py
+++ bidict/metadata.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2022 Joshua Bronson. All rights reserved.
+# Copyright 2009-2023 Joshua Bronson. All rights reserved.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,9 +7,9 @@
 """Define bidict package metadata."""
 
 
-__version__ = '0.22.1'
+__version__ = '0.22.2.dev0'
 __author__ = {'name': 'Joshua Bronson', 'email': 'jabronson@gmail.com'}
-__copyright__ = '© 2009-2022 Joshua Bronson'
+__copyright__ = '© 2009-2023 Joshua Bronson'
 __description__ = 'The bidirectional mapping library for Python.'
 __license__ = 'MPL 2.0'
 __url__ = 'https://bidict.readthedocs.io'
--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2009-2022 Joshua Bronson. All rights reserved.
+# Copyright 2009-2023 Joshua Bronson. All rights reserved.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
