fc90df74efa16628b0214645979b0f1a094930fd
Joshua Bronson <jabronson@gmail.com>
2023-02-20 08:50:17 -0500

    Provide development shells via "nix develop"
    On any of the following systems where "nix develop" is available,
    it can be run to enter a development shell where all nix-provided
    Python versions are available, and "tox" can be invoked to run the
    tests in each Python environment (as well as to lint and build docs):
    
    * x86_64-linux
    * aarch64-linux
    * x86_64-darwin
    * aarch64-darwin
    
    Tested on aarch64-darwin and x86_64-darwin.

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "bidict";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # Helper to provide system-specific attributes
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      forAllSystems = f: genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.pre-commit
+            pkgs.python311Packages.tox
+            pkgs.python311
+            pkgs.python310
+            pkgs.python39
+            pkgs.python38
+            pkgs.pypy39
+            pkgs.pypy38
+          ];
+        };
+      });
+    };
+}
