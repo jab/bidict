fcab6b43f78e67936b88b421ca86e9017b18ff12
Joshua Bronson <jabronson@gmail.com>
2023-02-20 08:50:17 -0500

    Provide development shell via "nix develop"
    On any of the following systems where "nix develop" is available,
    "nix develop" can be run to enter a development shell with several
    nix-provided Python versions available (namely, Python 3.8-3.11
    and PyPy 3.8-3.9), and "tox" can be invoked to run the tests in
    with each of those Python versions (as well as to lint and build
    docs as usual):
    
    * x86_64-linux
    * aarch64-linux
    * x86_64-darwin
    * aarch64-darwin
    
    Tested on aarch64-darwin and x86_64-darwin.

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "bidict";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # Helper to provide system-specific attributes
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      forAllSystems = f: genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.pre-commit
+            pkgs.python311Packages.pip
+            pkgs.python311Packages.tox
+            pkgs.python311
+            pkgs.python310
+            pkgs.python39
+            pkgs.python38
+            pkgs.pypy39
+            pkgs.pypy38
+          ];
+          shellHook = ''
+            if ! test -e .venv; then
+              python3.11 -m venv --upgrade-deps .venv
+              .venv/bin/pip install -r dev-deps/dev.txt
+              .venv/bin/pip install -e .
+            fi
+            source .venv/bin/activate
+          '';
+        };
+      });
+    };
+}
