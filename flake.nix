c85aab009837a7ffcd1c4efa9ce1a9255bc4e397
Joshua Bronson <jabronson@gmail.com>
2024-11-02 16:14:03 -0400

    switch from project.optional-dependencies to dependency-groups
    ...now that the latest version of uv supports it.
    
    Also update flake.lock to include latest revision of nixpkgs, which
    contains the latest version of uv.

--- flake.nix
+++ flake.nix
@@ -2,11 +2,14 @@
   description = "bidict";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    # Get pypy310 from a nixpkgs revision where its binary is in the binary cache
+    # to avoid building it from source.
+    pypy310.url = "github:NixOS/nixpkgs/d4f247e89f6e10120f911e2e2d2254a050d0f732";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, flake-utils, nixpkgs, pypy310 }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
@@ -19,14 +22,13 @@
           python311
           python310
           python39
-          pypy310
-          pypy39
-          uv  # bootstrap uv version (another version is installed in the dev env by this version)
+          uv  # Bootstrap version used to install a possibly-newer version when 'uv sync' is run (via uv.lock)
+        ] ++ [
+          pypy310.legacyPackages.${system}.pypy310
         ];
         shellHook = ''
           ./init_dev_env
           source ".venv/bin/activate"
-          echo "* Activated development virtualenv"
         '';
       };
     });
