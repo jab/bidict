58fb1c9c98ca62f0eede1467bd34c0f6cb14bb3a
Joshua Bronson <jabronson@gmail.com>
2025-01-05 19:11:30 -0500

    Get pypy310 from same nixpkgs revision
    since the associated binary is in the binary cache

--- flake.nix
+++ flake.nix
@@ -4,12 +4,9 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
-    # Get pypy310 from a nixpkgs revision where its binary is in the binary cache
-    # to avoid building it from source.
-    pypy310.url = "github:NixOS/nixpkgs/d4f247e89f6e10120f911e2e2d2254a050d0f732";
   };
 
-  outputs = { self, flake-utils, nixpkgs, pypy310 }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
@@ -22,9 +19,8 @@
           python311
           python310
           python39
+          pypy310
           uv  # Bootstrap version used to install a possibly-newer version when 'uv sync' is run (via uv.lock)
-        ] ++ [
-          pypy310.legacyPackages.${system}.pypy310
         ];
         shellHook = ''
           ./init_dev_env
