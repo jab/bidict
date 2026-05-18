84bceb129813c051db5564f1380dbeda320e6c0a
Joshua Bronson <jabronson@gmail.com>
2026-05-17 23:01:34 -0400

    optimize prek invocation + minor improvements

--- flake.nix
+++ flake.nix
@@ -11,20 +11,28 @@
     let
       pkgs = import nixpkgs { inherit system; };
     in {
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          prek
-          python314
-          python313
-          python312
-          python311
-          pypy3
-          uv  # Bootstrap version used to install a possibly-newer version when 'uv sync' is run (via uv.lock)
-        ];
-        shellHook = ''
-          ./init_dev_env
-          source ".venv/bin/activate"
-        '';
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            prek
+            python314
+            python313
+            python312
+            python311
+            pypy3
+            uv  # Bootstrap version used to install a possibly-newer version when 'uv sync' is run (via uv.lock)
+          ];
+          shellHook = ''
+            ./init_dev_env
+            source ".venv/bin/activate"
+          '';
+        };
+        lint = pkgs.mkShell {
+          packages = with pkgs; [prek];
+        };
+        update_deps = pkgs.mkShell {
+          packages = with pkgs; [prek uv];
+        };
       };
     });
 }
